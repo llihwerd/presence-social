7d8ec3083853453820d11d89b356892b8a8d7871
llihwerd <100llihwerd@gmail.com>
2024-01-22 17:06:33 -0500

    add show route to updates router

--- routes/updates.js
+++ routes/updates.js
@@ -4,11 +4,14 @@ import * as updatesCtrl from '../controllers/updates.js'
 const router = Router()
 
 
-// POST localhost:3000/performers
+// POST localhost:3000/updates
 router.post('/', updatesCtrl.create)
 
-// DELETE http://localhost:3000/profiles/:profileId
+// DELETE http://localhost:3000/updates/:updateId
 router.delete('/:updateId', updatesCtrl.deleteUpdate)
+
+// GET http://localhost:3000/updates/:updateId
+router.get('/:updateId', updatesCtrl.show)
 
 export {
   router
