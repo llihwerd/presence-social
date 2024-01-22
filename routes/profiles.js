5baf44c15b8b117ae75c3b0e9f0ae7985bb6400e
llihwerd <100llihwerd@gmail.com>
2024-01-22 13:43:24 -0500

    make all profiles view functional

--- routes/profiles.js
+++ routes/profiles.js
@@ -1,26 +1,13 @@
 import { Router } from 'express'
-import * as moviesCtrl from '../controllers/movies.js'
+import * as profilesCtrl from '../controllers/profiles.js'
 
 const router = Router()
 
 // GET http://localhost:3000/movies
 router.get('/', profilesCtrl.index)
-// GET http://localhost:3000/movies/new
-router.get('/new', moviesCtrl.new)
-// GET http://localhost:3000/movies/:movieId
-router.get('/:movieId', moviesCtrl.show)
-// GET http://localhost:3000/movies/:movieId/edit
-router.get('/:movieId/edit', moviesCtrl.edit)
-// POST http://localhost:3000/movies
-router.post('/', moviesCtrl.create)
-// POST http://localhost:3000/movies/:movieId/reviews
-router.post('/:movieId/reviews', moviesCtrl.createReview)
-// POST http://localhost:3000/movies/:movieId/performers
-router.post('/:movieId/performers', moviesCtrl.addToCast)
-// DELETE http://localhost:3000/movies/:movieId
-router.delete('/:movieId', moviesCtrl.delete)
-// PUT http://localhost:3000/movies/:movieId
-router.put('/:movieId', moviesCtrl.update)
+
+
+
 
 export {
   router
